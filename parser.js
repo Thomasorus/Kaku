beef66ee3aa589a0bd998435fc161e3fd1878983
Thomasorus <contact@thomasorus.com>
2020-05-05 19:05:27 +0200

    Finished parser for basic text

--- parser.js
+++ parser.js
@@ -1,38 +1,18 @@
-const demo = `
-# This is a title
-> Now I'm quoting stuff omagad
-## Damn, a second title
-This simple text now has *bold text* and _italic text_ and *more bold text*.
-How about a {https://duckduckgo.com, link, Link to duckduckgo} ?
-[https://i.imgur.com/oJXNeQm.jpg, Sailor Jupiter, She has some poses like Bruce Lee]
-
-test 0
-
-- List 1
-- List 2
-- List 3
-
-+ wow 1
-+ wow 2
 
 
-? test 1 : def 1
-? test 2 : def 2
-
-`
 let tempList = []
 let currentList = null
 
 function parser(text) {
-    text = text.split(/\n/g).filter(Boolean);
-    console.log({
-        text
-    })
+    text = text.split(/\n/g).filter(Boolean)
+    Object.keys(text).forEach(k => (!text[k] && text[k] !== undefined) && delete text[k]);
     let finalText = ""
     let count = 0;
 
     text.forEach(el => {
         count++
+        el = el.trim()
+        
         let firstChar = el.charAt(0)
         let content = null
         switch (firstChar) {
@@ -48,41 +28,38 @@ function parser(text) {
             case "-":
             case "?":
             case "+":
-                console.log(firstChar, currentList)
                 if (firstChar === currentList && count !== text.length || currentList === null && count !== text.length) {
                     content = populateList(firstChar, el)
                 } 
-                else if(count === text.length)  {
-                    content = populateList(firstChar, el) + lists(el)
+                else if(firstChar !== currentList &&  currentList.length > 0)  {
+                    content = lists(el)
+                    populateList(firstChar, el) 
                 }
                 else {
+                    populateList(firstChar, el) 
                     content = lists(el)
-                    content = content + populateList(firstChar, el)
                 }
                 break;
-            default:     
-                console.log(el)
-                content = endList(el)
+            default:
+                    content = endList(el)
                 break;
         }
 
-
-        //Bold
-        if (content.includes("*")) {
-            content = bold(el)
+        if(content) {
+            if (content.includes("*")) {
+                content = bold(content)
+            }
+    
+            if (content.includes("_")) {
+                content = italic(content)
+            }
+    
+            if (content.includes("{")) {
+                content = links(content)
+            }
+    
+            finalText = finalText + content
         }
-
-        //Italic
-        if (content.includes("_")) {
-            content = italic(el)
-        }
-
-        //Links
-        if (content.includes("{")) {
-            content = links(el)
-        }
-
-        finalText = finalText + content
     });
 
     return finalText
@@ -146,7 +123,7 @@ function paragraph(el) {
 function populateList(firstChar, el) {
     tempList.push(el)
     currentList = firstChar
-    return ""
+    return null
 }
 
 function titles(el) {
@@ -236,6 +213,28 @@ function italic(el) {
     return elem
 }
 
+
+const demo = document.querySelector('textarea').textContent;
+// demo = `# This is a title
+// > Now I'm quoting stuff omagad
+// ## Damn, a second title
+// This simple text now has *bold text* and _italic text_ and *more bold text*.
+// How about a {https://duckduckgo.com, link, Link to duckduckgo} ?
+// [https://i.imgur.com/oJXNeQm.jpg, Sailor Jupiter, She has some poses like Bruce Lee]
+
+// - Bullet list 1
+// - Bullet list 2
+// - Bullet list 3
+
+// + Ordered list 1
+// + Ordered list 2
+
+
+// ? Descriptive list 1 : Definition 1
+// ? Descriptive list 2 : Definition 2`
 const html = parser(demo);
-const body = document.querySelector('body');
-body.innerHTML = html;
+const formated = document.querySelector('.normal');
+const htmldemo = document.querySelector('.html');
+
+formated.innerHTML = html;
+htmldemo.append(html);
