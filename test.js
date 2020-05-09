a09c851c607ada0c7a40e444b8706f74b57ff106
thomas <thomas@macbook-pro-de-thomas.home>
2020-05-09 08:42:09 -0700

    Added some tests

--- test.js
+++ test.js
@@ -1,16 +1,23 @@
 const test = require('ava');
 const p = require("./parser_new.js");
 
-const demo = `
-I do some \`code\`.
-
-Sometimes with *bold*.
-
-Sometimes with _italic_ ?
-
-`
-
 test('Bold', t => {
-	const result = p.parser("mon test *qui* passe");
-	t.is(result, "mon test <strong>qui</strong> passe");
+	const result = p.parser("Sometimes with *bold* inside.");
+	t.is(result, "Sometimes with <strong>bold</strong> inside.");
+});
+
+
+test('code', t => {
+	const result = p.parser("I do some \`code\` text.");
+	t.is(result, "I do some <code>code</code> text.");
+});
+
+test('italic', t => {
+	const result = p.parser("Sometimes with _italic_ text?");
+	t.is(result, "Sometimes with <em>italic</em> text?");
+});
+
+test('paragraph', t => {
+	const result = p.parser(`\nI am a line\n\n`);
+	t.is(result, "<p>I am a line</p>");
 });
