44677920879453e20943cc119bd1f62875fc83be
Thomasorus <contact@thomasorus.com>
2020-06-02 23:50:27 +0200

    Added quotes and modified sperators in links, images, quotes

--- parser.js
+++ parser.js
@@ -1,33 +1,31 @@
 const symbol = [
-    // /^((?:[^\n]|\n(?! *\n))+)(?:\n *)+\n/g,
     /\*((?:\\[\s\S]|[^\\])+?)\*(?!\*)/g,
     /\_((?:\\[\s\S]|[^\\])+?)\_(?!\_)/g,
     /(\`+)([\s\S]*?[^`])\1(?!`)/g,
-    // /( *>[^\n]+(\n[^\n]+)*\n*)+\n{2,}/g,
     / *(#{1,6})([^\n]+?)#* *(?:\n *)+\n/g,
-    /\[((?:\\[\s\S]|[^\\])+?)\,((?:\\[\s\S]|[^\\])+?)\]/g,
-    /\{((?:\\[\s\S]|[^\\])+?)\,((?:\\[\s\S]|[^\\])+?)\}/g,
+    /\~((?:\\[\s\S]|[^\\])+?)\|((?:\\[\s\S]|[^\\])+?)\~/g,
+    /\[((?:\\[\s\S]|[^\\])+?)\|((?:\\[\s\S]|[^\\])+?)\]/g,
+    /\{((?:\\[\s\S]|[^\\])+?)\|((?:\\[\s\S]|[^\\])+?)\}/g,
 ]
 
 
 function parser(rawtText) {
     rawtText = rawtText.replace(/\r\n/g, "\n")
     rawtText = rawtText.split(/\n/g).filter(Boolean)
-
     let text = []
-    const regex = /^\#|^\{|^\[|^\*|^\_|^\`|^\>|^\-|^\+|^\?/g;
+    const regex = /^\#|^\{|^\[|^\*|^\_|^\`|^\~|^\-|^\+|^\?/g;
 
     for (let i = 0; i < rawtText.length; i++) {
         let el = rawtText[i];
         const firstChar = el.charAt(0)
-        if(regex.test(firstChar)) {
-            text.push(el)
+        if(firstChar.match(regex)) {
+            text.push(el + "\n\n")
         } else {
             text.push(`<p>${el}</p>`)
         }
     }
     
-    text = text.join('\n\n')
+    text = text.join('')
 
     for (let i = 0; i < symbol.length; i++) {
         const s = symbol[i];
@@ -49,9 +47,9 @@ function parser(rawtText) {
                     case "`":
                         text = createTypography(e, text, type, "code")
                         continue
-                    // case ">":
-                    //     text = createTypography(e, text, type, "blockquote")
-                    //     continue
+                    case "~":
+                        text = createQuote(e, text)
+                        continue
                     case "[":
                         text = createImages(e, text)
                         continue
@@ -79,49 +77,49 @@ function createTypography(el, text, type, tag) {
     return text.replace(el, html)
 }
 
+function createQuote(quote, text) {
+    let el = quote
+    el = el.replace(/\~/g, '')
+    el = el.split('|')
+    const citation = el[0].trim();
+    const url = `cite="${el[3].trim()}"`
+    const source = `, <cite>${el[2]}</cite>`
+    const author = `<footer>—${el[1].trim()}${source}</footer>`
+    const html = `
+        <blockquote ${url}>
+            <p>${citation}</p>
+            ${author}
+        </blockquote>
+    `;
+    return text.replace(quote, html)
+}
+
 function createImages(img, text) {
     let el = img
-    let imgElem = el.substring(
-        el.lastIndexOf("[") + 1,
-        el.lastIndexOf("]")
-    );
-    el = el.split(/\[|\]/)
-    let html = "";
-    el.forEach(e => {
-        if (e === imgElem) {
-            e = e.split(",")
-            let alt = e.length > 1 ? ` alt="${e[1].trim()}"` : ""
-            let img = `<img src="${e[0]}"${alt}></img>`
+    el = el.replace("[", "").replace("]", "")
+    const imgArr = el.split("|")
 
-            if (e.length > 2) {
-                let caption = `<figcaption>${e[2].trim()}</figcaption>`
-                e = `<figure>${img}${caption}</figure>`
-            } else {
-                e = img
-            }
-        }
-        html = html + e
-    });
+    let alt = imgArr.length > 1 ? ` alt="${imgArr[1].trim()}"` : ""
+    let imgHtml = `<img src="${imgArr[0].trim()}"${alt}></img>`
+
+    if (imgArr.length > 2) {
+        let caption = `<figcaption>${imgArr[2].trim()}</figcaption>`
+        imgHtml = `<figure>${imgHtml}${caption}</figure>`
+    } 
+
+    let html = imgHtml;
+
     return text.replace(img, html)
 }
 
 function createLink(link, text) {
     let el = link
-    let linkElem = el.substring(
-        el.lastIndexOf("{") + 1,
-        el.lastIndexOf("}")
-    );
-    el = el.split(/{|}/)
-
-    let html = "";
-    el.forEach(e => {
-        if (e === linkElem) {
-            e = e.split(",")
-            const aria = e.length > 2 ? ` aria-label="${e[2].trim()}"` : ""
-            e = `<a href="${e[0]}"${aria}>${e[1].trim()}</a>`
-        }
-        html = html + e
-    });
+    el = el.replace("{", "").replace("}", "")
+    linkElem = el.split("|")
+    const aria = linkElem.length > 2 ? ` aria-label="${linkElem[2].trim()}"` : ""
+    let html = `
+      <a href="${linkElem[0]}"${aria}>${linkElem[1].trim()}</a>
+    `;
     return text.replace(link, html)
 }
 
