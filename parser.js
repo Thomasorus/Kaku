245224b5a65f98ac325b92c89911c6f8471eece3
Thomasorus <contact@thomasorus.com>
2020-06-06 15:48:00 +0200

    Added support for lists

--- parser.js
+++ parser.js
@@ -6,31 +6,56 @@ const symbol = [
     /\~((?:\\[\s\S]|[^\\])+?)\,((?:\\[\s\S]|[^\\])+?)\~/g,
     /\[((?:\\[\s\S]|[^\\])+?)\,((?:\\[\s\S]|[^\\])+?)\]/g,
     /\{((?:\\[\s\S]|[^\\])+?)\,((?:\\[\s\S]|[^\\])+?)\}/g,
+    /\- ((?:\\[\s\S]|[^\\])+?)([^\n]*)/g,
+    /\+ ((?:\\[\s\S]|[^\\])+?)([^\n]*)/g,
+    /\? ((?:\\[\s\S]|[^\\])+?)([^\n]*)/g,
 ]
+
 
 
 function parser(rawtText) {
     rawtText = rawtText.replace(/\r\n/g, "\n")
-    rawtText = rawtText.split(/\n/g).filter(Boolean)
+    rawtText = rawtText.split(/\n/g)
     let text = []
     const regex = /^\#|^\{|^\[|^\*|^\_|^\`|^\~|^\-|^\+|^\?/g;
+    const regexList = /^\-|^\+|^\?/g;
 
+    let acc = []
     for (let i = 0; i < rawtText.length; i++) {
         let el = rawtText[i];
         const firstChar = el.charAt(0)
         if(firstChar.match(regex)) {
-            text.push(el + "\n\n")
+            if(firstChar.match(regexList)) {
+                acc.push(el)
+                if(i === rawtText.length - 1) {
+                    text.push(acc.join(''))
+                }
+            } else {
+                if(acc.length) {
+                    text.push(acc.join('') + "\n\n")
+                    acc.length = 0
+                }
+                text.push(el + "\n\n")
+            }
         } else {
-            text.push(`<p>${el}</p>`)
+            if(acc.length) {
+                text.push(acc.join('') + "\n\n")
+                acc.length = 0
+            }
+            if(el.length > 0){
+                text.push(`<p>${el}</p>`)
+
+            }
         }
     }
-    
+
     text = text.join('')
 
     for (let i = 0; i < symbol.length; i++) {
         const s = symbol[i];
         while (text.match(s)) {
             const match = text.match(s);
+            // console.log(match)
             const type = match[0].charAt(0)
             for (let t = 0; t < match.length; t++) {
                 const e = match[t];
@@ -56,6 +81,11 @@ function parser(rawtText) {
                     case "{":
                         text = createLink(e, text)
                         continue
+                    case "-":
+                    case "+":
+                    case "?":
+                        text = createList(e, text, type)
+                        continue;
                 }
                 return text
             }
@@ -65,9 +95,47 @@ function parser(rawtText) {
     return text
 }
 
+
+
+function createList(el, text, type) {
+    let tempList = el.split(type).filter(Boolean)
+    let listType = undefined
+    switch (type) {
+        case "-":
+            listType = "ul"
+            break;
+        case "+":
+            listType = "ol"
+            break;
+         case "?":
+            listType = "dl"
+            break;
+    }
+
+    let html = ""
+
+    if (listType === "ul" || listType === "ol") {
+        tempList.forEach(item => {
+            html = html + `<li>${item.substring(1).trim()}</li>`
+        });
+        html = `<${listType}>${html}</${listType}>\n`
+    } else {
+        tempList.forEach(item => {
+            item = item.split(":")
+            const term = item[0].substring(1).trim()
+            const definition = item[1].trim()
+            html = html + `<dt>${term}</dt><dd>${definition}</dd>`
+        });
+        html = `<${listType}>${html}</${listType}>\n`
+    }
+
+    return text.replace(el, html)
+
+}
+
 function createTitle(el, text) {
     const count = (el.match(/#/g)).length;
-    const html = `<h${count}>${el.substring(1 + count)}</h${count}>`
+    const html = `<h${count}>${el.substring(1 + count).trim()}</h${count}>`
     return text.replace(el, html)
 }
 
@@ -119,7 +187,6 @@ function createImages(img, text) {
     } 
 
     let html = imgHtml;
-    console.log(html)
     return text.replace(img, html)
 }
 
