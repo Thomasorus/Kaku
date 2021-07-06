24c2697ad4f14500c193bd60c07c48528cc936dc
Thomasorus <contact@thomasorus.com>
2021-07-06 19:38:43 +0200

    Finished parser

--- src/parser.js
+++ src/parser.js
@@ -8,12 +8,12 @@ function parser(text) {
 
         if(!codeTest) {
             let tempText = el
-                .replace(/^# (.*$)/gim, '<h1>$1</h1>') // h1 tag
-                .replace(/^## (.*$)/gim, '<h2>$1</h2>') // h2 tag
-                .replace(/^### (.*$)/gim, '<h3>$1</h3>') // h3 tag
-                .replace(/^#### (.*$)/gim, '<h4>$1</h4>') // h4 tag
-                .replace(/^##### (.*$)/gim, '<h5>$1</h5>') // h5 tag
-                .replace(/^###### (.*$)/gim, '<h6>$1</h6>') // h6 tag
+                .replace(/^(#) (.*$)/gim, function (char, item, item2) { return parseTitles(item, item2) }) // h1 tag
+                .replace(/^(##) (.*$)/gim, function (char, item, item2) { return parseTitles(item, item2) }) // h2 tag
+                .replace(/^(###) (.*$)/gim, function (char, item, item2) { return parseTitles(item, item2) }) // h3 tag
+                .replace(/^(####) (.*$)/gim, function (char, item, item2) { return parseTitles(item, item2) }) // h4 tag
+                .replace(/^(#####) (.*$)/gim, function (char, item, item2) { return parseTitles(item, item2) }) // h5 tag
+                .replace(/^(######) (.*$)/gim, function (char, item, item2) { return parseTitles(item, item2) }) // h6 tag
                 .replace(/^----$/gim, '<hr>') // hr tag
                 .replace(/\_(.*)\_/gim, '<em>$1</em>') // em text
                 .replace(/\*(.*)\*/gim, '<strong>$1</strong>') // strong text
@@ -25,7 +25,10 @@ function parser(text) {
                 .replace(/\(link:(.*)\)/gim, function (char, item) { return parseLinks(item) }) // links
                 .replace(/\(image:(.*)\)/gim, function (char, item) { return parseImage(item) }) // image
                 .replace(/\(video:(.*)\)/gim, function (char, item) { return parseVideo(item) }) // links
-                .replace(/\(audio:(.*)\)/gim, '<audio controls src="$1" type="audio/mpeg" preload="metadata"></audio>') // links
+                .replace(/\(audio:(.*)\)/gim, function (char, item) { 
+                    const mp3 = item.trim()
+                    return `<audio controls src="${mp3}" type="audio/mpeg" preload="metadata"></audio>`
+                 }) // links
                 .replace(/\(quote:(.*)\)/gim, function (char, item) {return parseQuote(item) }) // blockquote
 
             const htmlRegex = new RegExp("^\<(.*)\>", 'gim')
@@ -42,7 +45,15 @@ function parser(text) {
             const splitCode = el.trim().split("\n")
             splitCode.shift()
             splitCode.pop()
-            const tempText = `<pre><code>${splitCode.join("\n")}</code></pre>`
+            const cleanedCode = []
+            splitCode.forEach(el => {
+                if (el.includes('<')) {
+                    cleanedCode.push(el.replace(/</g, '<span><</span>'))
+                  } else {
+                    cleanedCode.push(el)
+                }
+            })
+            const tempText = `<pre><code>${cleanedCode.join("\n")}</code></pre>`
             if(tempText) {
                 parsedText += tempText
             }
@@ -53,8 +64,16 @@ function parser(text) {
         .replace(/<\/ul>\n<ul>/g, '')
         .replace(/<\/ol>\n<ol>/g, '')
         .replace(/<\/dl>\n<dl>/g, '')
+        .replace(/<p>[\s\S\n]<\/p>/gim, '')
 
     return cleanedText
+}
+
+function parseTitles(character, titleContent) {
+    const lvl = character.length
+    const titleId = titleContent.replace(/ /g, '-').trim().toLowerCase()
+    const html = `<h${lvl} id="${titleId}" style="position:relative;"><a href="#${titleId}" aria-label="${titleContent} permalink" style="display: inline-block;width: 100%;height: 100%;position: absolute;"></a>${titleContent}</h${lvl}>`
+    return html
 }
 
 function parseLinks(linkContent) {
@@ -64,26 +83,26 @@ function parseLinks(linkContent) {
     const labelData = /label:(.+?(?=text|title|$))/.exec(linkContent)
     const link = linkData ? `href="${linkData[1].trim()}"` : ""
     const text = textData ? textData[1].trim() : linkData[1].trim()
-    const title = titleData ? `title="${titleData[1].trim()}"` : ""
-    const label = labelData ?  `aria-label="${labelData[1].trim()}"` : ""
-    const html = `<a ${link} ${title} ${label}>${text}</a>`
+    const title = titleData ? ` title="${titleData[1].trim()}"` : ""
+    const label = labelData ?  ` aria-label="${labelData[1].trim()}"` : ""
+    const html = `<a ${link}${title}${label}>${text}</a>`
     return html
 }
 
 function parseImage(imgContent) {    
-    const linkData = /^(.+?(?= |\)$))/.exec(imgContent)
+    const linkData = /^(.+?(?=figcaption|alt|$))/.exec(imgContent)
     const altData = /alt:(.+?(?=figcaption|$))/.exec(imgContent)
     const figcaptionData = /figcaption:(.+?(?=alt|$))/.exec(imgContent)
     const matches = imgContent.split(/alt:(\.+?(?=( figcaption:|\)$)))|figcaption:(\.+?(?=( alt:|\)$)))/g)
-    const link = linkData[1] ? `src="${linkData[1].trim()}"` : ""
-    const alt = altData[1] ? `alt="${altData[1].trim()}"` : ""
-    const figcaption = figcaptionData[1] ? `<figcaption>${figcaptionData[1].trim()}</figcaption>` : false
+    const link = linkData ? `src="${linkData[1].trim()}"` : ""
+    const alt = altData ? `alt="${altData[1].trim()}"` : ""
+    const figcaption = figcaptionData ? `<figcaption>${figcaptionData[1].trim()}</figcaption>` : false
 
     if (figcaption) {
-        const html = `<figure><img ${link} ${alt}></img>${figcaption}</figure>`
+        const html = `<figure><img loading="lazy" ${link} ${alt}>${figcaption}</figure>`
         return html
     } else {
-        const html = `<img ${link} ${alt}></img>`
+        const html = `<img loading="lazy" ${link} ${alt}>`
         return html
     }
 }
@@ -92,10 +111,10 @@ function parseVideo(videoContent) {
     const array = videoContent.split(" ")
     const matches = array.filter(Boolean);
     const link = matches[0] ? `src="${matches[0].trim()}"` : ""
-    const controls = matches[1] ? "autoplay loop mute" : "controls"
+    const controls = matches[1] ? "autoplay playsinline loop mute" : "controls playsinline"
     const format = /\.mp4|\.webm|\.mov/.exec(matches[0])
-    const source = `type="video/${format}"`
-    const html = `<video ${link} ${controls} ${source} preload="metadata" playsinline></video>`
+    const source = `type="video/${format.toString().slice(1)}"`
+    const html = `<video ${controls} preload="metadata" ${link} ${source}></video>`
     return html
 }
 
